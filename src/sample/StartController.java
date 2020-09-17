97c656c82ed49fbae7f572f5633eb0c23a38395c
Irma Dedić <idedic2@etf.unsa.ba>
2020-09-17 02:14:35 +0100

    Initial commit

---
+++ src/sample/StartController.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class StartController {
+    public ChoiceBox<String>choiceAlgorithm;
+
+    public void confirmAlgorithmAction(ActionEvent actionEvent){
+
+    }
+}
