919bafb2f489f7370886fb32b4315c743554e74f
Irma Dedić <idedic2@etf.unsa.ba>
2020-09-17 03:28:52 +0100

    Implementiran Middle Square

---
+++ src/sample/MiddleSquareController.java
@@ -0,0 +1,156 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+public class MiddleSquareController {
+    public RadioButton radioWith;
+    public RadioButton radioWithout;
+    public Label lblText;
+    public TextField fieldSeed;
+    public TextArea textareaNumbers;
+
+    private boolean allDigits(String str) {
+        return str.chars().allMatch(Character::isDigit);
+    }
+
+    private void showAlert(String title, String headerText, Alert.AlertType type) {
+        Alert error = new Alert(type);
+        error.setTitle(title);
+        error.setHeaderText(headerText);
+        error.show();
+    }
+
+    public void radioChange(ActionEvent actionEvent) {
+        if (radioWithout.isSelected()) {
+            lblText.setDisable(true);
+            fieldSeed.setDisable(true);
+        } else {
+            lblText.setDisable(false);
+            fieldSeed.setDisable(false);
+        }
+    }
+
+    public void closeMiddleSquare(ActionEvent actionEvent) {
+        Stage stage = (Stage) lblText.getScene().getWindow();
+        stage.close();
+        Stage oldstage = new Stage();
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/start.fxml"));
+            StartController startController = new StartController();
+            loader.setController(startController);
+            root = loader.load();
+            oldstage.setTitle("Dobrodošli");
+            oldstage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            oldstage.setResizable(false);
+            oldstage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /*middlesquare System.out.println("Unesite sjeme ");
+        Scanner ulaz = new Scanner(System.in);
+        Long sjeme = ulaz.nextLong();
+       ArrayList<Long>list=new ArrayList<>();
+       while(true){
+           Long rez=middleSquare(sjeme);
+           if(list.contains(rez))
+               break;
+           list.add(rez);
+           sjeme=rez;
+       }
+       for(Long l: list)
+           System.out.println(l);
+
+        */
+    public Long middleSquare(Long sjeme) {
+        String stringSjeme = sjeme.toString();
+        int brojCifara = stringSjeme.length();
+        Long kvadriranoSjeme = sjeme * sjeme;
+        String kvadriranoSjemeString = kvadriranoSjeme.toString();
+        String novoSjeme = "";
+        if (kvadriranoSjemeString.length() == (brojCifara * 2)) {
+
+            novoSjeme = kvadriranoSjemeString.substring(brojCifara / 2, brojCifara + brojCifara / 2);
+        } else {
+            kvadriranoSjemeString = "0" + kvadriranoSjemeString;
+            novoSjeme = kvadriranoSjemeString.substring(brojCifara / 2, brojCifara + brojCifara / 2);
+        }
+        return Long.parseLong(novoSjeme);
+    }
+
+    @FXML
+    public void initialize() {
+        fieldSeed.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (!fieldSeed.getText().trim().isEmpty()) {
+                    if (!allDigits(fieldSeed.getText())) {
+                        fieldSeed.getStyleClass().removeAll("ispravnoPolje");
+                        fieldSeed.getStyleClass().add("neispravnoPolje");
+                    } else {
+                        fieldSeed.getStyleClass().removeAll("neispravnoPolje");
+                        fieldSeed.getStyleClass().add("ispravnoPolje");
+                    }
+                } else {
+                    fieldSeed.getStyleClass().removeAll("ispravnoPolje");
+                    fieldSeed.getStyleClass().add("neispravnoPolje");
+                }
+            }
+        });
+
+    }
+
+    public void generateAction(ActionEvent actionEvent) {
+        if (radioWith.isSelected()) {
+            if (fieldSeed.getText().trim().isEmpty()) {
+                //sveOk = false;
+                showAlert("Greška", "Unesite početnu vrijednost", Alert.AlertType.ERROR);
+                return;
+            }
+
+            if (!allDigits(fieldSeed.getText())) {
+                //sveOk = false;
+                showAlert("Greška", "Početna vrijednost mora sadržavati isključivo cifre", Alert.AlertType.ERROR);
+                return;
+            }
+            ArrayList<Long> list = new ArrayList<>();
+            Long sjeme = Long.parseLong(fieldSeed.getText());
+            while (true) {
+                Long rez = middleSquare(sjeme);
+                if (list.contains(rez))
+                    break;
+                list.add(rez);
+                sjeme = rez;
+            }
+            textareaNumbers.setText(list.toString());
+        } else {
+            ArrayList<Long> list = new ArrayList<>();
+            Long sjeme = Long.parseLong(String.valueOf(675248));
+            while (true) {
+                Long rez = middleSquare(sjeme);
+                if (list.contains(rez))
+                    break;
+                list.add(rez);
+                sjeme = rez;
+            }
+            textareaNumbers.setText(list.toString());
+
+        }
+
+    }
+}
--- src/sample/StartController.java
+++ src/sample/StartController.java
@@ -9,11 +9,27 @@ import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
 import java.io.IOException;
-
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class StartController {
     public ChoiceBox<String>choiceAlgorithm;
 
     public void confirmAlgorithmAction(ActionEvent actionEvent){
-
+        if(choiceAlgorithm.getSelectionModel().getSelectedItem().equals("Middle Square")){
+            Stage stage = (Stage) choiceAlgorithm.getScene().getWindow();
+            Parent root = null;
+            try {
+                stage.close();
+                Stage primaryStage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/middleSquare.fxml"));
+                root = loader.load();
+                primaryStage.setTitle("Middle Square algoritam");
+                primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                primaryStage.initModality(Modality.APPLICATION_MODAL);
+                primaryStage.setResizable(false);
+                primaryStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
